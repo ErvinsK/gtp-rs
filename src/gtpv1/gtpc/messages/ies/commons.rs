e189faad4a6cddf1b6eab64a9c53e8230e601893
Ervins Kampans <ervins.kampans@1nce.com>
2022-08-06 00:37:01 +0300

    Quick fix

--- src/gtpv1/gtpc/messages/ies/commons.rs
+++ src/gtpv1/gtpc/messages/ies/commons.rs
@@ -7,30 +7,3 @@ pub trait IEs {
     fn unmarshal (buffer:&[u8]) -> Result<Self, GTPV1Error> where Self:Sized;
     fn len (&self) -> usize; // Total IE length including Type+Value for TV messages, Type+Length+Value for TLV messages
 }
-
-// Implementation of Empty IE for internal needs
-
-// Empty IE Implementation
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct EmptyIE {}
-
-impl Default for EmptyIE {
-    fn default() -> EmptyIE {
-        EmptyIE {}         
-    }
-}
-
-impl IEs for EmptyIE {
-    fn marshal (&self, buffer: &mut Vec<u8>) {
-        ()
-    }
-
-    fn unmarshal (buffer:&[u8]) -> Result<Self, GTPV1Error> where Self:Sized {
-            Ok(EmptyIE::default())       
-    }
-    
-    fn len (&self) -> usize {
-       0 
-    }
-}
